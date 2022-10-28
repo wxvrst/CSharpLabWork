303a07ca7c56ecd3fb216fbeb34955eefabbd23b
wxvrst <70475271+wxvrst@users.noreply.github.com>
2022-10-28 11:01:25 +0500

    Update Form1.cs

--- CSharpLabWork/Form1.cs
+++ CSharpLabWork/Form1.cs
@@ -10,34 +10,42 @@ namespace CSharpLabWork
         private void button1_Click(object sender, EventArgs e)
         {
             //data entry validation
+            bool condition = true;
             if (textBox1.Text.Length == 0)
             {
                 MessageBox.Show("First name was not entered");
+                condition = false;
             }
-            if(textBox2.Text.Length == 0)
+            if (textBox2.Text.Length == 0)
             {
                 MessageBox.Show("last name was not entered");
+                condition = false;
             }
-            if(textBox3.Text.Length == 0)
+            if (textBox3.Text.Length == 0)
             {
                 MessageBox.Show("Middle name was not entered");
+                condition = false;
             }
-            if(comboBox1.Text.Length == 0)
+            if (comboBox1.Text.Length == 0)
             {
                 MessageBox.Show("Group was not chosen");
+                condition = false;
             }
             if (!radioButton1.Checked && !radioButton2.Checked)
             {
                 MessageBox.Show("Complexity was not chosen");
+                condition = false;
             }
-
-            Form2 tf = new Form2(
-                textBox1.Text + " " + textBox2.Text + " " + textBox3.Text,
-                comboBox1.Text,
-                numericUpDown1.Value,
-                radioButton1.Checked
-                ) ;
-            tf.ShowDialog();
+            if (condition)
+            {
+                Form2 tf = new Form2(
+                    textBox1.Text + " " + textBox2.Text + " " + textBox3.Text,
+                    comboBox1.Text,
+                    numericUpDown1.Value,
+                    radioButton1.Checked
+                    );
+                tf.ShowDialog();
+            }
         }
     }
 }
