367f71b5fbaaa48144dfdce7e8e667b6a93d663e
Mour-Yam <70475271+Mour-Yam@users.noreply.github.com>
2022-11-25 01:58:57 +0500

    d

--- CSharpLabWork/Ellipse.cs
+++ CSharpLabWork/Ellipse.cs
@@ -9,33 +9,19 @@ namespace CSharpLabWork
     internal class Ellipse : GraphObject
     {
         static Random r = new Random();
-        public int AAxis { get; init; }
-        public int BAxis { get; init; }
         public Ellipse(int maxPointX, int maxPointY, int pointX = 0, int pointY = 0)
-            : base(pointX, pointY)
+            : base(maxPointX, maxPointY, pointX, pointY)
         {
-            AAxis = r.Next(10, maxPointX / 2);
-            BAxis = r.Next(10, maxPointY / 2);
-            if (pointX == 0 && pointY == 0)
-            {
-                PointX = r.Next(maxPointX - AAxis);
-                PointY = r.Next(maxPointY - BAxis);
-            }
-            else
-            {
-                PointX = pointX;
-                PointY = pointY;
-            }
         }
         public override void Draw(Graphics g)
         {
-            g.FillEllipse(brush, PointX, PointY, AAxis, BAxis);
-            g.DrawEllipse(FigurePen, PointX, PointY, AAxis, BAxis);
+            g.FillEllipse(brush, PointX, PointY, Width, Height);
+            g.DrawEllipse(FigurePen, PointX, PointY, Width, Height);
         }
         public override bool ContainsPoint(MouseEventArgs e)
         {
-            //(x-x0)^2/a^2 + (y-y0)^2/b^2 <=1
-            return Math.Pow((e.X - PointX) / AAxis, 2) + Math.Pow((e.Y - PointY) / BAxis, 2) <= 1; 
+
+            return Math.Pow((e.X - PointX) / Width, 2) + Math.Pow((e.Y - PointY) / Height, 2) <= 1;
         }
     }
 }
--- CSharpLabWork/GraphObject.cs
+++ CSharpLabWork/GraphObject.cs
@@ -16,16 +16,31 @@ namespace CSharpLabWork
         public bool Selected { get; set; }
         public int PointX { get; set; }
         public int PointY { get; set; }
+        public int Width { get; init; }
+        public int Height { get; init; }
         public Pen FigurePen { get; set; } = new(Brushes.Red, 2);
         public SolidBrush brush;
 
         public abstract bool ContainsPoint(MouseEventArgs e);
         public abstract void Draw(Graphics g);
-        public GraphObject(int pointX = 0, int pointY = 0)
+        public GraphObject(int maxPointX, int maxPointY, int pointX = 0, int pointY = 0)
         {
             color = Color.FromArgb(r.Next(255), r.Next(255), r.Next(255));
             if (PointX < 0 || PointY < 0)  { throw new ArgumentException("point < 0 !"); }
             brush = new SolidBrush(color);
+
+            Width = r.Next(maxPointX/8, maxPointX / 2);
+            Height = r.Next(maxPointY/8, maxPointY / 2);
+            if (pointX == 0 && pointY == 0)
+            {
+                PointX = r.Next(maxPointX - Width);
+                PointY = r.Next(maxPointY - Height);
+            }
+            else
+            {
+                PointX = pointX;
+                PointY = pointY;
+            }
         }
 
     }
